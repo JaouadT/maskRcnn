f4fad98397298bc33337faad1e23176873201a8e
Jaouad Tagnamas <jaouad@Jaouads-MacBook-Pro.local>
2021-11-02 12:23:12 +0100

    initial

---
+++ mrcnn/defs.py
@@ -0,0 +1,118 @@
+from mrcnn.config import Config
+from mrcnn import model as modellib
+from mrcnn import visualize
+import mrcnn
+import numpy as np
+import colorsys
+import argparse
+import imutils
+import random
+import cv2
+import os
+
+from matplotlib import pyplot
+from matplotlib.patches import Rectangle
+
+from os import listdir
+from xml.etree import ElementTree
+from numpy import zeros
+from numpy import asarray
+from mrcnn.utils import Dataset
+
+from mrcnn.model import MaskRCNN
+
+
+class KangarooDataset(Dataset):
+    # load the dataset definitions
+    def load_dataset(self, dataset_dir, is_train=True):
+        
+        # Add classes. We have only one class to add.
+        self.add_class("dataset", 1, "kangaroo")
+        
+        # define data locations for images and annotations
+        images_dir = dataset_dir + '/images/'
+        annotations_dir = dataset_dir + '/annots/'
+        
+        # Iterate through all files in the folder to 
+        #add class, images and annotaions
+        for filename in listdir(images_dir):
+            
+            # extract image id
+            image_id = filename[:-4]
+            
+            # skip bad images
+            if image_id in ['00090']:
+                continue
+            # skip all images after 150 if we are building the train set
+            if is_train and int(image_id) >= 150:
+                continue
+            # skip all images before 150 if we are building the test/val set
+            if not is_train and int(image_id) < 150:
+                continue
+            
+            # setting image file
+            img_path = images_dir + filename
+            
+            # setting annotations file
+            ann_path = annotations_dir + image_id + '.xml'
+            
+            # adding images and annotations to dataset
+            self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
+
+    # extract bounding boxes from an annotation file
+    def extract_boxes(self, filename):
+        
+        # load and parse the file
+        tree = ElementTree.parse(filename)
+        # get the root of the document
+        root = tree.getroot()
+        # extract each bounding box
+        boxes = list()
+        for box in root.findall('.//bndbox'):
+            xmin = int(box.find('xmin').text)
+            ymin = int(box.find('ymin').text)
+            xmax = int(box.find('xmax').text)
+            ymax = int(box.find('ymax').text)
+            coors = [xmin, ymin, xmax, ymax]
+            boxes.append(coors)
+        
+        # extract image dimensions
+        width = int(root.find('.//size/width').text)
+        height = int(root.find('.//size/height').text)
+        return boxes, width, height
+
+    # load the masks for an image
+    """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+     """
+    def load_mask(self, image_id):
+        # get details of image
+        info = self.image_info[image_id]
+        
+        # define anntation  file location
+        path = info['annotation']
+        
+        # load XML
+        boxes, w, h = self.extract_boxes(path)
+       
+        # create one array for all masks, each on a different channel
+        masks = zeros([h, w, len(boxes)], dtype='uint8')
+        
+        # create masks
+        class_ids = list()
+        for i in range(len(boxes)):
+            box = boxes[i]
+            row_s, row_e = box[1], box[3]
+            col_s, col_e = box[0], box[2]
+            masks[row_s:row_e, col_s:col_e, i] = 1
+            class_ids.append(self.class_names.index('kangaroo'))
+        return masks, asarray(class_ids, dtype='int32')
+
+    # load an image reference
+    def image_reference(self, image_id):
+        info = self.image_info[image_id]
+        print(info)
+        return info['path']
